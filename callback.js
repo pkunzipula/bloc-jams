c6faf25fd221136cdfd4710aeb306db7c9ba20d2
James Capra <jcapra76@gmail.com>
2017-08-06 15:06:27 -0700

    Checkpoint 9 DOM Events: complete!

---
+++ callback.js
@@ -0,0 +1,7 @@
+var houseStark = ["Ned", "Catelyn", "Robb", "Sansa", "Arya", "Bran", "Rickon"];
+
+function battleCry(element) {
+    console.log(element + " cries: For Winterfell!");
+}
+
+houseStark.forEach(battleCry);
--- scripts/landing.js
+++ scripts/landing.js
@@ -1,35 +1,34 @@
-var animatePoints = function(){
-    var points = document.getElementsByClassName('point');
 
-/*    var revealFirstPoint = function() {
-        points[0].style.opacity = 1;
-        points[0].style.transform = "scaleX(1) translateY(0)";
-        points[0].style.msTransform = "scaleX(1) translateY(0)";
-        points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-    };
+var pointsArray = document.getElementsByClassName('point');
 
-    var revealSecondPoint = function() {
-        points[1].style.opacity = 1;
-        points[1].style.transform = "scaleX(1) translateY(0)";
-        points[1].style.msTransform = "scaleX(1) translateY(0)";
-        points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
-
-    var revealThirdPoint = function() {
-        points[2].style.opacity = 1;
-        points[2].style.transform = "scaleX(1) translateY(0)";
-        points[2].style.msTransform = "scaleX(1) translateY(0)";
-        points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
-*/
-    var revealPoint = function() {
-        for(var i = 0; i < points.length; i++) {
-            points[i].style.opacity = 1;
-            points[i].style.transform = "scale(1)";
-            points[i].style.msTransform = "scale(1)";
-            points[i].style.WebkitTransform = "scale(1)";
-        }
-    }
-    revealPoint();
+var revealPoint = function(element) {
+    element.style.opacity = 1;
+    element.style.transform = "scale(1)";
+    element.style.msTransform = "scale(1)";
+    element.style.WebkitTransform = "scale(1)";
 }
-animatePoints();
+
+/*function revealPoint(element) {
+    element.style.opacity = 1;
+    element.style.transform = "scale(1)";
+    element.style.msTransform = "scale(1)";
+    element.style.WebkitTransform = "scale(1)";
+}*/
+var animatePoints = function(points){
+    forEach(points, revealPoint);
+}
+
+window.onload = function() {
+    if(window.innerHeight > 950) {
+        animatePoints(pointsArray);
+        //pointsArray.forEach(revealPoint);
+    }
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    window.addEventListener('scroll', function(event) {
+        if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
+            //pointsArray.forEach(revealPoint);
+        }
+    });
+}
