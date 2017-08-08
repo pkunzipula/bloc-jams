88166e1ec2b4cb2105c87d8cff3eb2ea70da6fd6
James Capra <jcapra76@gmail.com>
2017-08-08 10:23:37 -0700

    Checkpoint 11 DOM Album View: Completed!

---
+++ scripts/album.js
@@ -0,0 +1,96 @@
+// Album One
+var albumList = [
+ {
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/11.png',
+    songs: [
+        { title: 'Blue', duration: '4:26' },
+        { title: 'Green', duration: '3:14' },
+        { title: 'Red', duration: '5:01' },
+        { title: 'Pink', duration: '3:21' },
+        { title: 'Magenta', duration: '2:15' }
+    ]
+},
+
+{
+    title: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/02.png',
+    songs: [
+        { title: 'Hello, Operator', duration: '1:01' },
+        { title: 'Ring, ring, ring', duration: '5:01' },
+        { title: 'Fits in your pocket', duration: '3:21' },
+        { title: 'Can you hear me now?', duration: '3:14' },
+        { title: 'Wrong phone number', duration: '2:15' }
+    ]
+},
+
+{
+    title: 'Fidge Pickles',
+    artist: 'The Capra Family Singers',
+    label: 'Goat',
+    year: '1985',
+    albumArtUrl: 'assets/images/album_covers/03.png',
+    songs: [
+        { title: 'I Don\t Know What This Is, But I\'m Going to Set My Drink On It', duration: '2:01' },
+        { title: 'Making Love Out of Common Household Items', duration: '5:01' },
+        { title: 'I\'ll Never Be Your Pizza Burnin\'', duration: '3:21' },
+        { title: 'Wookin\' Pa Nub', duration: '3:14' },
+        { title: 'Feel Like Making Flan', duration: '2:15' }
+    ]
+}
+];
+
+var createSongRow = function(songNumber, songName, songLength) {
+    var template =
+    '<tr class="album-view-song-item">'
++   '   <td class="song-item-number">' + songNumber + '</td>'
++   '   <td class="song-item-title">' + songName + '</td>'
++   '   <td class="song-item-duration">' + songLength + '</td>'
++   '</tr>'
+;
+    return template;
+};
+
+var setCurrentAlbum = function(album) {
+    // #1
+    var albumTitle = document.getElementsByClassName('album-view-title')[0];
+    var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+    var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+    var albumImage = document.getElementsByClassName('album-cover-art')[0];
+    var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
+    // #2
+    albumTitle.firstChild.nodeValue = album.title;
+    albumArtist.firstChild.nodeValue = album.artist;
+    albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
+    albumImage.setAttribute('src', album.albumArtUrl);
+
+    // #3
+    albumSongList.innerHTML = '';
+
+    // #4
+    for (var i = 0; i < album.songs.length; i++) {
+        albumSongList.innerHTML += createSongRow(i+1, album.songs[i].title, album.songs[i].duration);
+    }
+
+};
+
+window.onload = function() {
+    setCurrentAlbum(albumList[0]);
+}
+
+var albumCover = document.getElementsByClassName('album-cover-art')[0];
+var n = 0;
+albumCover.addEventListener("click", function() {
+        n++;
+        if(n == 3) {
+            n = 0;
+        }
+        setCurrentAlbum(albumList[n]);
+});
