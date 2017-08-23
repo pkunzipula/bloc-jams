cd9bcdb0f2ce048a7e202bbed77d659cd46fc2e9
James Capra <jcapra76@gmail.com>
2017-08-22 22:18:52 -0700

    here

--- scripts/album.js
+++ scripts/album.js
@@ -1,6 +1,16 @@
 var setSong = function(songNumber) {
+    if (currentSoundFile) {
+        currentSoundFile.stop();
+    }
     currentlyPlayingSongNumber = parseInt(songNumber);
     currentSongFromAlbum = currentAlbum.songs[songNumber - 1];
+    
+    currentSoundFile = new buzz.sound(currentSongFromAlbum.audioUrl, {
+        formats: ['mp3'],
+        preload: true
+    });
+    
+    setVolume(currentVolume);
 };
 
 var getSongNumberCell = function(number) {
@@ -26,13 +36,21 @@ var createSongRow = function(songNumber, songName, songLength) {
         };
         
         if(currentlyPlayingSongNumber !== songNumber) {
-            $(this).html(pauseButtonTemplate);
             setSong(songNumber);
+            currentSoundFile.play();
+            $(this).html(pauseButtonTemplate);
+            currentSongFromAlbum = currentAlbum.songs[songNumber -1];
+            updatePlayerBarSong();
         } else if(currentlyPlayingSongNumber === songNumber) {
-            $(this).html(playButtonTemplate);
-            $('main-controls .play-pause').html(playerBarPlayButton);
-            currentlyPlayingSongNumber = null;
-            currentSongFromAlbum = null;
+            if (currentSoundFile.isPaused()) {
+                $(this).html(pauseButtonTemplate);
+                $('.main-controls .play-pause').html(playerBarPauseButton);
+                currentSoundFile.play();
+            } else {
+                $(this).html(playButtonTemplate);
+                $('.main-controls .play-pause').html(playerBarPlayButton);
+                currentSoundFile.pause();
+            }
         }
     };
     
@@ -86,10 +104,15 @@ var trackIndex = function(album, song) {
     return album.songs.indexOf(song);
 };
 
+
+
 var nextSong = function() {
     var currentSongIndex = trackIndex(currentAlbum, currentSongFromAlbum);
         
     currentSongIndex++;
+    
+    setSong(currentSongIndex + 1);
+    currentSoundFile.play();
     
     if (currentSongIndex >= currentAlbum.songs.length) {
         currentSongIndex = 0;
@@ -113,6 +136,9 @@ var prevSong = function() {
     var currentSongIndex = trackIndex(currentAlbum, currentSongFromAlbum);
     
     currentSongIndex--;
+    
+    setSong(currentSongIndex + 1);
+    currentSoundFile.play();
 
     if (currentSongIndex < 0) {
         currentSongIndex = currentAlbum.songs.length - 1;
@@ -139,6 +165,13 @@ var updatePlayerBarSong = function() {
     $('.main-controls .play-pause').html(playerBarPauseButton);
 };
 
+var setVolume = function(volume) {
+    if (currentSoundFile) {
+        currentSoundFile.setVolume(volume);
+    }
+};
+
+
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
 var playerBarPlayButton = '<span class="ion-play"></span>';
@@ -147,9 +180,12 @@ var playerBarPauseButton = '<span class="ion-pause"></span>';
 var currentAlbum = null;
 var currentlyPlayingSongNumber = null;
 var currentSongFromAlbum = null;
+var currentSoundFile = null;
+var currentVolume = 80;
 
 var $previousButton = $('.main-controls .previous');
 var $nextButton = $('.main-controls .next');
+
 
 $(function() {
     setCurrentAlbum(albumPicasso);
